95ed938738ec6416a6e39407a97ea8586c391cf0
brogli <28339986+brogli@users.noreply.github.com>
2018-08-29 12:18:53 +0200

    Added example scene for unity

--- Assets/Example MidiClock/MidiClockExample.cs
+++ Assets/Example MidiClock/MidiClockExample.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MidiTest : MonoBehaviour
-{
-    public GameObject cube;
-
-    
+public class MidiclockExample : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(MidiJack.MidiMaster.GetMidiClockCounter);
+        float clock = MidiJack.MidiMaster.GetMidiClockCounter();
+        transform.localScale = new Vector3(clock* 0.5f, clock * 0.5f, 1);
 
     }
-
-  
-
 }
